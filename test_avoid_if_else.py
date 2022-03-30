d3a636aab7ca6b2be21b6ed24176165fbb5dd487
phulikely <phulikely@gmail.com>
2022-03-30 21:10:57 +0700

    Passing same arguments to multiple functions #3 (Using Class)

--- test_avoid_if_else.py
+++ test_avoid_if_else.py
@@ -1,6 +1,17 @@
 from avoid_if_else_operation import *
 
 
+def main():
+    action_id = 4
+    name = 'test 123456'
+    width = 13579
+    height = 2468
+    length = 7777
+    using_dict = UsingDict(action_id=action_id, name=name, width=width, height=height, length=length)
+    result = using_dict.show_info_about_selection()
+    print(result)
+
+
 class UsingDict:
     def __init__(self, action_id, name, width, height, length):
         self.action_id = action_id
@@ -18,17 +29,6 @@ class UsingDict:
             4: lambda: smooth(**args)
         }
         return info_dict.get(self.action_id, default_func)()
-
-
-def main():
-    action_id = 4
-    name = 'test 123456'
-    width = 13579
-    height = 2468
-    length = 7777
-    using_dict = UsingDict(action_id=action_id, name=name, width=width, height=height, length=length)
-    result = using_dict.show_info_about_selection()
-    print(result)
 
 
 if __name__ == '__main__':
