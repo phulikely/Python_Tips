906ebe8fa2b87cef55f78faf01f966e0893086a3
phulikely <phulikely@gmail.com>
2022-07-13 23:01:30 +0700

    add using_git.py

---
+++ using_git.py
@@ -0,0 +1,28 @@
+# For just one repo, go into to the relevant repo DIR and:
+git config user.name "Your Name Here"
+git config user.email your@email.example
+
+
+#For (global) default email (which is configured in your ~/.gitconfig):
+git config --global user.name "Your Name Here"
+git config --global user.email your@email.example
+
+
+# You can check your Git settings with: 
+git config user.name && git config user.email
+
+-------------------------------------------------------
+
+# Show current status
+git stash show
+# If nothing then
+git stash
+
+# Do something and apply your changes(which you have been saved by git stash)
+git stash apply
+
+# Show again
+git stash show
+
+# Clear all stash
+git stash clear
